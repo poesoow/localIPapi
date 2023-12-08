45aa858722c8b5a5496646f84ecb619793089094
poesoow <119300922+poesoow@users.noreply.github.com>
2023-12-09 08:19:59 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors');
+
+const port = process.env.PORT || 3000;
+// express 는 함수이므로, 반환값을 변수에 저장한다.
+const app = express()
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({
+    success: true,
+    localIP: req.ip
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server is listening at localhost:${port}`);
+});
